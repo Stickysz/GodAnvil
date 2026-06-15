866f6c9077d6a27adea4094159a557a6ebf879ea
Stickysz <163073484+Stickysz@users.noreply.github.com>
2026-06-15 19:58:00 +0800

    Create BlockListener.java

---
+++ src/main/java/me/myat/godanvil/listener/BlockListener.java
@@ -0,0 +1,53 @@
+package me.myat.godanvil.listener;
+
+import me.myat.godanvil.GodAnvilPlugin;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+public class BlockListener implements Listener {
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        ItemStack item = event.getItemInHand();
+
+        if (!item.hasItemMeta()) {
+            return;
+        }
+
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta.getPersistentDataContainer().has(
+                CraftListener.GOD_ANVIL_KEY,
+                PersistentDataType.BOOLEAN
+        )) {
+            Location location = event.getBlockPlaced().getLocation();
+            GodAnvilPlugin.getInstance().getGodAnvils().add(location);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        Location location = block.getLocation();
+
+        if (!GodAnvilPlugin.getInstance().getGodAnvils().contains(location)) {
+            return;
+        }
+
+        event.setDropItems(false);
+
+        GodAnvilPlugin.getInstance().getGodAnvils().remove(location);
+
+        block.getWorld().dropItemNaturally(
+                location,
+                CraftListener.createGodAnvil()
+        );
+    }
+}
