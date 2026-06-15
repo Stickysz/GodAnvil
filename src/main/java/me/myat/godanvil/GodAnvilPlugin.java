178849fd02a9721116a70641ab8112fcc351a29c
Stickysz <163073484+Stickysz@users.noreply.github.com>
2026-06-15 19:50:17 +0800

    Update GodAnvilPlugin.java

--- src/main/java/me/myat/godanvil/GodAnvilPlugin.java
+++ src/main/java/me/myat/godanvil/GodAnvilPlugin.java
@@ -1,16 +1,27 @@
 package me.myat.godanvil;
 
+import me.myat.godanvil.listener.CraftListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GodAnvilPlugin extends JavaPlugin {
 
+    private static GodAnvilPlugin instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+
+        CraftListener.registerRecipe();
+
         getLogger().info("GodAnvil has been enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("GodAnvil has been disabled!");
+    }
+
+    public static GodAnvilPlugin getInstance() {
+        return instance;
     }
 }
